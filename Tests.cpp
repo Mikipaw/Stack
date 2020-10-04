0e28b973ea40f51e72c4ad08cf79a1e27082209f
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-10-05 01:25:35 +0300

    Add files via upload

---
+++ Tests.cpp
@@ -0,0 +1,17 @@
+//
+// Created by pawlo on 02.10.2020.
+//
+
+
+#include "Tests.h"
+
+void First_test(){
+    Stack<int> stk;
+    stk = Stack<int>(5);
+    stk.push(2);
+    stk.push(4);
+    assert(stk[1] == 4);
+    stk.Destruct();
+
+    fprintf(stderr, "First test is OK!\n");
+}
