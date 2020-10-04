541133469e0c9afccf60e05d96c69f78935b5aa1
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-10-05 01:23:28 +0300

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -4,7 +4,14 @@
 
 #ifndef STACK_TESTS_H
 #define STACK_TESTS_H
+#pragma once
+#include "Stack.h"
+
+void First_test();
+
+
 void AllTests(){
-    return;
+    First_test();
+    fprintf(stderr, "All tests are OK!\n");
 }
 #endif //STACK_TESTS_H
