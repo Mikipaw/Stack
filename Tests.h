93a02229ac60df3a2e13fae09139f7b572d13913
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-10-06 23:14:34 +0300

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -1,17 +1,20 @@
 //
 // Created by mikipaw on 29.09.2020.
 //
-
+#pragma once
 #ifndef STACK_TESTS_H
 #define STACK_TESTS_H
-#pragma once
 #include "Stack.h"
 
+#define DIE fprintf(stderr, "Assertion failed!\nERROR on line %d\n", __LINE__);
+
+#define asserted || DIE
+
 void First_test();
+void Second_test();
+void Third_test();
 
 
-void AllTests(){
-    First_test();
-    fprintf(stderr, "All tests are OK!\n");
-}
+void AllTests();
 #endif //STACK_TESTS_H
+
