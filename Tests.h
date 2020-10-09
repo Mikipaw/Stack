b66a8866d2e855cc2f4f88e8ce5257822ce3df92
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-10-09 16:38:04 +0300

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -1,7 +1,6 @@
 //
 // Created by mikipaw on 29.09.2020.
 //
-#pragma once
 #ifndef STACK_TESTS_H
 #define STACK_TESTS_H
 #include "Stack.h"
@@ -10,11 +9,38 @@
 
 #define asserted || DIE
 
+/*!
+ * @test First_test().
+ * @brief Test checks class methods
+ */
 void First_test();
+
+/*!
+ * @test Second_test().
+ * @brief Test checks typeid(type).name().
+ */
 void Second_test();
+
+/*!
+ * @test Third_test().
+ * @brief Test checks charsum() function.
+ */
 void Third_test();
+
+/*!
+ * @test Third_test().
+ * @brief Test checks stack with doubles.
+ */
+void Fourth_test();
+
+/*!
+ * @test Third_test().
+ * @brief Test checks stack with strings.
+ */
+void Fifth_test();
+
 
 
 void AllTests();
-#endif //STACK_TESTS_H
 
+#endif //STACK_TESTS_H
