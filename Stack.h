00b9bdd34ca0ced354d628f5ac29d9b42632384b
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-10-09 16:34:34 +0300

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -1,7 +1,6 @@
 //
 // Created by mikipaw on 29.09.2020.
 //
-#pragma once
 #ifndef STACK_STACK_H
 #define STACK_STACK_H
 
@@ -12,8 +11,26 @@
 #include <cassert>
 #include <cstdio>
 #include <stdexcept>
+#include <cmath>
 
 const long long STRANGE_NUMBER = 0xDEADBEEF;
+
+enum ERRORS{
+    SUCCESS,                            //0
+    INVALID_POINTER,                    //1
+    INVALID_SIZE,                       //2
+    CANARY_SCREAM,                      //3
+    UNAVAILABLE_ELEMENTS_WAS_CHANGED,   //4
+    UNACCEPTABLE_HASH,                  //5
+    UNKNOWN_ERROR,                      //6
+};
+
+/*!@function charsum(const char* string)
+ * @brief This function finds sum of chars of the string
+ * @param string
+ * @return an integer - sum of chars
+ */
+int charsum(const char* string);
 
 
 /*! \class Stack
@@ -30,19 +47,16 @@ private:
 
 public:
     Stack() = default;
+
     explicit
     Stack(size_t new_capacity):
         capacity (new_capacity),
-        size     (0){
-        data = (T*) calloc(capacity, sizeof(T));
-
+        size     (0),
+        data     (new T[new_capacity]),
+        HASH     (charsum(typeid(T).name()) + new_capacity) {
+        fill();
     }
 
-    explicit
-    Stack(T* new_data, size_t number_of_elements):
-        capacity (number_of_elements),
-        size     (number_of_elements),
-        data     (new_data){}
 
     int
     StackError(int error_number);
@@ -59,24 +73,18 @@ public:
 
 
     /*!
-     *  @method T pop().
+     *  @method bool pop(T* element).
      *  @brief  Removes last element.
-     *  It shrinks the Stack by one.
+     *  @param element - T*
+     *  It shrinks the Stack by one. Deleted item are in element after.
      *
-     *  @return a T - the element that was destroyed
+     *  @return a bool - true if success, false if not
      */
-    T
+    bool
+    pop(T* element);
+
+    bool
     pop();
-
-
-    /*!
-     *  @brief  Access to the data contained in the Stack.
-     *  @param index The index of the element for which data should be
-     *  accessed.
-     *  @return  Reference to data.
-     */
-    T&
-    operator[](size_t index);
 
     const T&
     at(int index) const;
@@ -90,21 +98,47 @@ public:
     void
     Destruct();
 
+    ~Stack() {
+        size = -1;
+        capacity = -1;
+        free(data);
+        //*data = 13;
+    }
+
+
     size_t  Get_Size()      const;
     size_t  Get_Capacity()  const;
     T *     Get_Data()      const;
 
 private:
 
+/*!
+    *  @method void fill(size_t new_capacity).
+    *  @brief  This method fills Stack after constructor call.
+    *  @param new_capacity a size_t.
+    *
+    *  @return nothing
+    */
+    void fill();
+
     /*!
-    *  @method void Expand(bool* flag).
+    *  @brief  Access to the data contained in the Stack.
+    *  @param index The index of the element for which data should be
+    *  accessed.
+    *  @return  Reference to data.
+    */
+    T&
+    operator[](size_t index);
+
+    /*!
+    *  @method void Expand(bool* success).
     *  @brief  This method expands block of memory.
-    *  @param flag a bool* - true if all is ok, false if expand was unsuccessful.
+    *  @param success a bool* - true if all is ok, false if expand was unsuccessful.
     *
     *  @return nothing
     */
     void
-    Expand(bool* flag);
+    Expand(bool* success);
 
     /*!
     *  @method void Decrease().
@@ -137,17 +171,20 @@ private:
     T* data = nullptr;
     size_t size = 0;
     size_t capacity = 0;
+    size_t HASH = 0;
 
     const long long canary2 = STRANGE_NUMBER;
 };
 
-
-/*!@function charsum(const char* string)
- * @brief This function finds sum of chars of the string
- * @param string
- * @return an integer - sum of chars
+/*!
+ * @function str_cmp(const char* str1, const char* str2)
+ * @brief Function compares 2 strings
+ * @param str1
+ * @param str2
+ * @return true if strings are equal, false if not
  */
-int charsum(const char* string);
+int str_cmp(const char* str1, const char* str2);
+
 
 template<typename T>
 void Stack_info(const Stack<T>& stk);
@@ -159,70 +196,170 @@ void Print_template_type(const T& var_name);
 template<typename T>
 void Stack<T>::Destruct(){
     this->Stack_OK();
-    assert(this);
     free(data);
-    *data = 13;
     size = -1;
     capacity = -1;
 }
 
 template<typename T>
-T Stack<T>::pop(){
-    this->Stack_OK();
-    assert(this);
+bool Stack<T>::pop(T* element){
+    if(!this->Stack_OK()) return false;
 
     if(size == 0) {
         fprintf(stderr, "Logic error at line %d:\n"
                         "It is impossible to delete an element from empty stack\n", __LINE__);
-        exit(6);
+        return false;
     }
+
+    if (HASH != capacity + charsum(typeid(T).name()) + pow(size*1.0, (size+capacity)%8))
+        StackError(UNACCEPTABLE_HASH);
 
     if(size - 1 < capacity/4) Decrease();
     size--;
 
-    T result = data[size];
-    data[size] = 0;
+    *element = data[size];
+
+    int sw = charsum(typeid(T).name());
+    switch (sw) {
+        case 105:   //integer
+            data[size] = -661;
+            break;
+
+        case 99:    //char
+            data[size] = 0;
+            break;
+
+        case 254:   //const char*
+            data[size] = (T) (int) "";
+            break;
+
+        case 179:   //char*
+            data[size] = (T) (int) "";
+            break;
+
+        case 102:   //float
+            data[size] = 1111,1111;
+            break;
+
+        case 100:   //double
+            data[size] = 1111.1111;
+            break;
+
+        default:
+            break;
+
+    }
 
     this->Stack_OK();
-    return result;
+    HASH = capacity + charsum(typeid(T).name()) + pow(size*1.0, (size+capacity)%8);
+    return true;
+}
+
+template<typename T>
+bool Stack<T>::pop(){
+    if(!this->Stack_OK()) return false;
+
+    if(size == 0) {
+        fprintf(stderr, "Logic error at line %d:\n"
+                        "It is impossible to delete an element from empty stack\n", __LINE__);
+        return false;
+    }
+
+    if (HASH != capacity + charsum(typeid(T).name()) + pow(size*1.0, (size+capacity)%8))
+        StackError(UNACCEPTABLE_HASH);
+
+    if(size - 1 < capacity/4) Decrease();
+    size--;
+
+    int sw = charsum(typeid(T).name());
+
+    switch (sw) {
+        case 105:   //integer
+            data[size] = -661;
+            break;
+
+        case 99:    //char
+            data[size] = 0;
+            break;
+
+        case 254:   //const char*
+            data[size] = (T) (int) "";
+            break;
+
+        case 179:   //char*
+            data[size] = (T) (int) "";
+            break;
+
+        case 102:   //float
+            data[size] = 1111,1111;
+            break;
+
+        case 100:   //double
+            data[size] = 1111.1111;
+            break;
+
+        default:
+            break;
+
+    }
+
+    this->Stack_OK();
+    HASH = capacity + charsum(typeid(T).name()) + pow(size*1.0, (size+capacity)%8);
+    return true;
 }
 
 template<typename T>
 bool Stack<T>::push(T item){
-    this->Stack_OK();
-    assert(this);
-    bool flag = true;
-    if (size == capacity) Expand(&flag);
+    if(!this->Stack_OK()) return false;
+
+    bool push_success = true;
+
+    if (HASH != capacity + charsum(typeid(T).name()) + pow(size*1.0, (size+capacity)%8))
+        StackError(UNACCEPTABLE_HASH);
+
+    if (size == capacity)
+        Expand(&push_success);
+    if(!push_success)
+        return push_success;
+
     data[size++] = item;
     this->Stack_OK();
-    return flag;
+    HASH = capacity + charsum(typeid(T).name()) + pow(size*1.0, (size+capacity)%8);
+    return push_success;
 }
 
 template<typename T>
 int Stack<T>::StackError(int error_number){
     switch (error_number) {
         case 1:
-            fprintf(stderr, "Bad pointer!\n");
+            fprintf(stderr, "Invalid pointer!\n");
             break;
 
         case 2:
-            fprintf(stderr, "Size is bad!\n");
+            fprintf(stderr, "Invalid size!\n");
             break;
 
         case 3:
-            fprintf(stderr, "Canary was hit!\n");
+            fprintf(stderr, "Canary was hit!\nCanary1 = %ld\nCanary2 = %ld\n", canary1, canary2);
             break;
+
         case 4:
-            fprintf(stderr, "Unused elements was changed!");
+            fprintf(stderr, "Unavailable elements was changed!");
             break;
+
+        case 5:
+            fprintf(stderr, "Unacceptable hash!");
+            break;
+
 
         default:
             fprintf(stderr, "Unknown error!\n");
             Stack_info(*this);
-            exit(11);
+            Destruct();
+            return error_number;
     }
     Stack_info(*this);
-    exit(1);
+    return 1;
 }
 
 template<typename T>
@@ -230,19 +367,49 @@ T& Stack<T>::operator[](size_t index){
     if(index < 0 || index >= size) {
         fprintf(stderr, "Out of range error at line %d:\n"
                         "Index %d is out of range!\n", __LINE__, index);
-        exit(3);
+        return nullptr;
     }
 
     return data[index];
 }
 
 template<typename T>
-void Stack<T>::Expand(bool* flag) {
+void Stack<T>::Expand(bool* success) {
     (capacity*=2)++;
     data = (T*) realloc(data, capacity*sizeof(T));
-    if (data == nullptr) *flag = false;
+    if (data == nullptr) *success = false;
 
-    for (int i = size; i < capacity; ++i) data[i] = 0;
+    int sw = charsum(typeid(T).name());
+    for (int i = size; i < capacity; ++i) {
+        switch (sw) {
+            case 105:   //integer
+                data[i] = -661;
+                break;
+
+            case 99:    //char
+                data[i] = 0;
+                break;
+
+            case 254:   //const char*
+                data[i] = (T) (int) "";
+                break;
+
+            case 179:   //char*
+                data[i] = (T) (int) "";
+                break;
+
+            case 102:   //float
+                data[i] = 1111,1111;
+                break;
+
+            case 100:   //double
+                data[i] = 1111.1111;
+                break;
+
+            default:
+                return;
+        }
+    }
 }
 
 template<typename T>
@@ -253,38 +420,69 @@ void Stack<T>::Decrease() {
 
 template<typename T>
 bool Stack<T>::Stack_OK() {
-    int ERR = 0;
 
     if(this == nullptr) {   //NULL POINTER
-        ERR = 1;
-        StackError(ERR);
+        StackError(INVALID_POINTER);
         return false;
     }
 
     if(size > capacity) {   // BAD SIZE
-        ERR = 2;
-        StackError(ERR);
+        StackError(INVALID_SIZE);
         return false;
     }
 
     if(this->data == nullptr) { //NULL POINTER
-        ERR = 1;
-        StackError(ERR);
+        StackError(INVALID_POINTER);
         return false;
     }
 
     if(canary1 != STRANGE_NUMBER || canary2 != STRANGE_NUMBER) { //CANARY WAS HIT
-        ERR = 3;
-        StackError(ERR);
+        StackError(CANARY_SCREAM);
         return false;
     }
 
-    for(int i = size; i < capacity; ++i) {
-        if (!Zero(data[i])) {
-            ERR = 4;
-            StackError(ERR);
-        }
+    int sw = charsum(typeid(data[0]).name());
+    for (int i = size; i < capacity; ++i) {
+        switch (sw) {
+            case 105:   //integer
+                if(data[i] == -661) break;
+                StackError(UNAVAILABLE_ELEMENTS_WAS_CHANGED);
+                return false;
 
+
+            case 99:    //char
+                if(data[i] != 0) break;
+                StackError(UNAVAILABLE_ELEMENTS_WAS_CHANGED);
+                return false;
+
+
+            case 254:   //const char*
+                if(str_cmp((const char*) (int) data[i], "")) break;
+                StackError(UNAVAILABLE_ELEMENTS_WAS_CHANGED);
+                return false;
+
+
+            case 179:   //char*
+                if(str_cmp((const char*) (int) data[i], "")) break;
+                StackError(UNAVAILABLE_ELEMENTS_WAS_CHANGED);
+                return false;
+
+
+            case 102:   //float
+                if(Zero(data[i] - 1111.1111)) break;
+                StackError(UNAVAILABLE_ELEMENTS_WAS_CHANGED);
+                return false;
+
+
+            case 100:   //double
+                if(Zero(data[i] - 1111.1111)) break;
+                StackError(UNAVAILABLE_ELEMENTS_WAS_CHANGED);
+                return false;
+
+
+            default:
+                break;
+        }
     }
     return true;
 }
@@ -304,29 +502,67 @@ const T& Stack<T>::at(int index) const {
 }
 
 template<typename T>
+void Stack<T>::fill(){
+    int sw = charsum(typeid(T).name());
+    for (int i = 0; i < capacity; ++i) {
+        switch (sw) {
+            case 105:   //integer
+                data[i] = -661;
+                break;
+
+            case 99:    //char
+                data[i] = 0;
+                break;
+
+            case 254:   //const char*
+                data[i] = (T) (int) "";
+                break;
+
+            case 179:   //char*
+                data[i] = (T) (int) "";
+                break;
+
+            case 102:   //float
+                data[i] = 1111,1111;
+                break;
+
+            case 100:   //double
+                data[i] = 1111.1111;
+                break;
+
+            default:
+                return;
+        }
+    }
+}
+
+template<typename T>
 void Print_template_type(const T& var_name){
     int a = charsum(typeid(var_name).name());
     switch (a) {
-        case 105:
+        case 105:   //integer
             printf("%d " , var_name);
             break;
 
-        case 99:
+        case 99:    //char
             printf("%c " , var_name);
             break;
 
-        case 254:
+        case 254:   //const char*
             printf("%s ", var_name);
             break;
 
-        case 179:
+        case 179:   //char*
             printf("%s ", var_name);
+            break;
 
-        case 102:
+        case 102:   //float
             printf("%f", var_name);
+            break;
 
-        case 100:
+        case 100:   //double
             printf("%lf", var_name);
+            break;
 
         default:
             printf("unknown type\n");
@@ -334,15 +570,6 @@ void Print_template_type(const T& var_name){
             return;
     }
 }
-
-/*!
- * @function str_cmp(const char* str1, const char* str2)
- * @brief Function compares 2 strings
- * @param str1
- * @param str2
- * @return true if strings are equal, false if not
- */
-int str_cmp(const char* str1, const char* str2);
 
 template<typename T>
 void Stack_info(const Stack<T>& stk){
@@ -364,11 +591,15 @@ void Stack_info(const Stack<T>& stk){
 template<typename T>
 bool Stack<T>::Zero(T element){
     const char* typ = typeid(element).name();
-    if(str_cmp(typ, "f") || str_cmp(typ, "i")) return element == 0;
-    if(str_cmp(typ, "CKp") || str_cmp(typ, "Cp")) return str_cmp(element, "");
+    if(str_cmp(typ, typeid(int).name())) return element == 0;
+
+    if(str_cmp(typ, typeid(const char*).name()) || str_cmp(typ, typeid(char*).name()))
+        return str_cmp((const char*) (int) element, "");
 
     return (element < 10e-6 && element > -10e-6);
 }
+
+
 
 
 #endif //STACK_STACK_H
