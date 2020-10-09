2db1032ce6acfe51e44d5a8659ca953221f85ce6
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-10-09 16:37:12 +0300

    Update Tests.cpp

--- Tests.cpp
+++ Tests.cpp
@@ -6,24 +6,30 @@
 #include "Tests.h"
 
 void AllTests(){
-    //First_test();
-    //Second_test();
+    First_test();
+    Second_test();
     Third_test();
+    Fourth_test();
+    //Fifth_test();
     fprintf(stderr, "All tests are OK!\n");
 }
 
+//!Checks Stack's methods
 void First_test(){
     Stack<int> stk = Stack<int>(5);
     stk.push(2);
     stk.push(4);
     stk.push(88);
 
-    stk[1] == 4             asserted;
-    stk[2] == 88            asserted;
+    stk.at(1) == 4    asserted;
+    stk.at(2) == 88   asserted;
     stk.Get_Capacity() == 5 asserted;
     stk.Get_Size() == 3     asserted;
 
-    stk.pop() == 88         asserted;
+    int check = 0;
+
+    stk.pop(&check)         asserted;
+    check == 88             asserted;
     stk.Get_Size() == 2     asserted;
     stk.Get_Capacity() == 5 asserted;
 
@@ -38,11 +44,11 @@ void First_test(){
     stk.Get_Capacity() == 11 asserted;
 
     Stack_info(stk);
-    stk.Destruct();
 
     fprintf(stderr, "First test is OK!\n");
 }
 
+//!Checks str_cmp with typeid
 void Second_test(){
     const char* str = "String";
 
@@ -50,6 +56,7 @@ void Second_test(){
     else fprintf(stderr, "Second test was failed!\n%s != PKc\n", typeid(str).name());
 }
 
+//!Checks charsum of types
 void Third_test() {
     int i = 0;
     double d = 0;
@@ -65,12 +72,33 @@ void Third_test() {
     int sum5 = charsum(typeid(f).name());
     int sum6 = charsum(typeid(c).name());
 
-    sum1 == 105 asserted;
-    sum2 == 100 asserted;
-    sum3 == 254 asserted;
-    sum4 == 179 asserted;
-    sum5 == 102 asserted;
-    sum6 == 99  asserted;
+    sum1 == 'i'             asserted;
+    sum2 == 'd'             asserted;
+    sum3 == 'P' + 'c' + 'K' asserted;
+    sum4 == 'P' + 'c'       asserted;
+    sum5 == 'f'             asserted;
+    sum6 == 'c'             asserted;
 
     fprintf(stderr, "Third test is OK!\n");
+}
+
+void Fourth_test(){
+    Stack<double> stak(3);
+
+    stak.push(2.32);
+    stak.push(2);
+
+    stak.Get_Size()     == 2 asserted;
+    stak.Get_Capacity() == 3 asserted;
+
+    double rvar = 0;
+
+    stak.pop(&rvar) == 1 asserted;
+    stak.pop();
+    (rvar - 2) < 10e-6 && (rvar - 2) > -10e-6 asserted;
+
+    stak.Get_Size() == 0 asserted;
+    Stack_info(stak);
+    //stak.Destruct();
+    fprintf(stderr, "Fourth test is OK!\n");
 }
